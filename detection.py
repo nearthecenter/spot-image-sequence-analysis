3b1b8f00453d676360f2345ad41d5ec672380e69
Gello Guiam <acguiam@gmail.com>
2021-05-17 22:08:06 +0800

    Add initial blob detection code
    - Use connectedComponentsWithStats function from cv2 library

--- detection.py
+++ detection.py
@@ -1,15 +1,19 @@
 import cv2
 
 def blob_detection(image):
-    return
+    output = cv2.connectedComponentsWithStats(image, cv2.CV_32S)
+    (numLabels, labels, stats, centroids) = output
+    
+    cv2.imwrite('sample_output.jpg', image)
 
 
 def main():
     image = cv2.imread('asset/sample.jpg')
 
-    img_gray = cv2.cvtColor(image, cv2.BGR2GRAY)
-    
-    blob_detection(image)
+    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, img_binary = cv2.threshold(img_gray, 200, 255, cv2.THRESH_BINARY)
+
+    blob_detection(img_binary)
 
 
 if __name__ == "__main__":
