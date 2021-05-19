8c80b54122ff1762b08bbb1124df4ec265c54bc6
Bems <63622141+bemsorc@users.noreply.github.com>
2021-05-19 11:57:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import cv2
+def morphological(frame1, frame2):
+    motionDiff = cv.absdiff(frame1, frame2)
+    gray = cv.cvtColor(motionDiff, cv.COLOR_BGR2GRAY)
+    blur = cv.GaussianBlur(gray, (5,5), 0)
+
+    _, th = cv.threshold(blur, 20, 255, cv.THRESH_BINARY)
+    dilate = cv.dilate(th, None, iterations=3)
+    cntr, _ = cv.findContours(dilate, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    for c in cntr:
+        [intX, intY, intW, intH] = cv.boundingRect(c)
+        cv.rectangle(frame1,  (intX, intY),(intX+intW, intY+intH),(0,255,0),2)
+    return frame1
